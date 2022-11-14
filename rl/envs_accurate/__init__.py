fd3ff7ad048face14557b661ee004b644f63ef7e
John Mark Agosta <joagosta@microsoft.com>
2022-11-14 10:33:35 -0800

    remove duplicate dir

--- rl/envs_accurate/__init__.py
+++
@@ -1,9 +0,0 @@
-# module for a gym environment
-# that wraps an off-line data set. 
-from gym.envs.registration import register
-
-register(
-    id='BogoEnv-Acc-v0',
-    entry_point='envs_accurate.bogo_accurate:BogoEnv',
-    max_episode_steps=300
-)
